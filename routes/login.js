13a35fb39540e17904f9d7842ef8c80277a75e71
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-05-07 21:10:25 -0600

    3.4

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const passwordInput = document.getElementById('password');
-    const mostrarContrasenaBtn = document.getElementById('mostrarContrasena');
-  
-    mostrarContrasenaBtn.addEventListener('click', () => {
-      if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        mostrarContrasenaBtn.innerHTML = '<i class="material-symbols-rounded">visibility</i>';
-      } else {
-        passwordInput.type = 'password';
-        mostrarContrasenaBtn.innerHTML = '<i class="material-symbols-rounded">visibility_off</i>';
-      }
-    });
-  });
-  
+//routes/login.js
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const authMiddleware = require('../middlewares/authMiddleware'); // Middleware para proteger rutas
+const carritoController = require('../controllers/carritoController');
+
+// Ruta para mostrar el formulario de login
+router.get('/', (req, res) => {
+  res.render('login', { title: 'Iniciar sesión', user: req.user != null ? `${req.user.nombre}` : '' });
+});
+
+
+router.post('/', passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: true
+}), async (req, res) => {
+  // Si se autentica correctamente, crea un token JWT
+  const token = authMiddleware.generateToken(req.user.id);
+
+  res.cookie('token', token, { httpOnly: true, secure: false });
+
+  let carrito = res.locals.carrito;
+
+  if(carrito){
+    await duplicarCarritoEnDB(req.user.id, carrito);
+  }
+
+  res.redirect('/carrito');
+});
+
+
+async function duplicarCarritoEnDB(usuarioId, carrito) {
+  for (const producto in carrito) {
+    console.log('Producto: ' + carrito[producto].nombre);
+
+    await carritoController.agregarProducto(usuarioId, carrito[producto].id, carrito[producto].cantidad);
+  }
+}
 
 module.exports = router;
