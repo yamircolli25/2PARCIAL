323ec41a1f948a94a0e683b71f906e67ef53bc38
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-05-07 21:39:17 -0600

    3.5

--- routes/login.js
+++ routes/login.js
@@ -1,41 +1,10 @@
 //routes/login.js
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const authMiddleware = require('../middlewares/authMiddleware'); // Middleware para proteger rutas
-const carritoController = require('../controllers/carritoController');
 
 // Ruta para mostrar el formulario de login
 router.get('/', (req, res) => {
-  res.render('login', { title: 'Iniciar sesión', user: req.user != null ? `${req.user.nombre}` : '' });
+  res.render('login', { title: 'Iniciar sesión'});
 });
-
-
-router.post('/', passport.authenticate('local', {
-  failureRedirect: '/login',
-  failureFlash: true
-}), async (req, res) => {
-  // Si se autentica correctamente, crea un token JWT
-  const token = authMiddleware.generateToken(req.user.id);
-
-  res.cookie('token', token, { httpOnly: true, secure: false });
-
-  let carrito = res.locals.carrito;
-
-  if(carrito){
-    await duplicarCarritoEnDB(req.user.id, carrito);
-  }
-
-  res.redirect('/carrito');
-});
-
-
-async function duplicarCarritoEnDB(usuarioId, carrito) {
-  for (const producto in carrito) {
-    console.log('Producto: ' + carrito[producto].nombre);
-
-    await carritoController.agregarProducto(usuarioId, carrito[producto].id, carrito[producto].cantidad);
-  }
-}
 
 module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 //Llama a los archivos js
 const index = require('./index');
+const login = require('./login');
 
 //Muestra la vista al usuario con el js y pug
 router.use('/', index);
+router.use('/login', login);
 
 module.exports = router;
