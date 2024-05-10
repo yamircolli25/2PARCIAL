0d04ee629a92d263a4851100232210c1e2708d73
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-05-09 19:15:32 -0600

    5.5

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ const index = require('./index');
 const login = require('./login');
 const registro = require('./registro');
 const logout = require('./logout');
+const converted = require('./converted');
 
 
 //Muestra la vista al usuario con el js y pug
@@ -13,6 +14,7 @@ router.use('/', index);
 router.use('/login', login);
 router.use('/registro',registro);
 router.use('/logout',logout);
+router.use('/converted',converted);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,59 @@ app.use(express.json());
 //Ruta raiz de la ejecución
 app.use('/', router);
 
+// Configuración de Multer para cargar imágenes
+const upload = multer({ dest: 'index/' });
+
+// Ruta para renderizar el formulario
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Ruta para manejar la solicitud de conversión de imagen
+app.post('/index', upload.single('image'), (req, res) => {
+    // Verificar si se cargó una imagen
+    if (!req.file) {
+        return res.status(400).send('No se ha proporcionado una imagen.');
+    }
+
+    // Obtener el formato seleccionado del cuerpo de la solicitud
+    const format = req.body.format;
+
+    // Procesar la imagen con Sharp
+    sharp(req.file.path)
+        .toFormat(format)
+        .toFile(path.join(__dirname, 'converted', `converted.${format}`), (err, info) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error al procesar la imagen');
+            } else {
+                // Renderizar la vista de imagen convertida
+                res.render('converted', { format });
+            }
+        });
+});
+
+
+// Servir imágenes convertidas
+app.use('/converted', express.static(path.join(__dirname, 'converted')));
+
+// Ruta para descargar la imagen convertida
+app.get('/download', (req, res) => {
+    const format = req.query.format;
+
+    // Imprimir el formato en la consola para verificar que se está pasando correctamente
+    console.log('Formato:', format);
+
+    // Verificar si se proporciona un formato válido
+    if (!format) {
+        return res.status(400).send('No se ha proporcionado un formato de imagen válido.');
+    }
+
+    const file = path.join(__dirname, 'converted', `converted.${format}`);
+    res.download(file);
+});
+
+
 //Inicia el servidor
 const port = 3000;
 app.listen(port, () =>{
