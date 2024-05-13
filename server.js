1f9af90cbd26f51b220495f018f88330cc137c56
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-05-13 10:39:48 -0600

    6.1

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const router = require('./routes/routes');
 const sharp = require('sharp');
 const multer = require('multer');
+const pool = require('./MySQL/conexion'); // Importa el pool de conexiones desde db.js
+
+
+require('dotenv').config(); // Cargar las variables de entorno
+
 
 const app = express();
 
@@ -11,7 +17,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 //midleware 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Configurar el middleware de sesión
+app.use(session({
+    secret: 'mi_secreto', // Deberías cambiar esto por una cadena de caracteres aleatoria
+    resave: false,
+    saveUninitialized: true
+  }));
 
 //Ruta raiz de la ejecución
 app.use('/', router);
@@ -25,7 +39,7 @@ app.get('/', (req, res) => {
 });
 
 // Ruta para manejar la solicitud de conversión de imagen
-app.post('/index', upload.single('image'), (req, res) => {
+app.post('/upload', upload.single('image'), (req, res) => {
     // Verificar si se cargó una imagen
     if (!req.file) {
         return res.status(400).send('No se ha proporcionado una imagen.');
@@ -48,6 +62,45 @@ app.post('/index', upload.single('image'), (req, res) => {
         });
 });
 
+
+// Ruta para manejar la solicitud de registro de usuario
+app.post('/registrar-usuario', (req, res) => {
+    const { usuario, email, password } = req.body;
+  
+    // Insertar usuario en la base de datos usando el pool de conexiones
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.error('Error al obtener conexión de la base de datos:', err);
+        res.status(500).send('Error interno del servidor');
+      } else {
+        const query = 'INSERT INTO users (usuario, correo, contrasenia_hash) VALUES (?, ?, ?)';
+        connection.query(query, [usuario, email, password], (err, result) => {
+          connection.release(); // Liberar la conexión de vuelta al pool
+  
+          if (err) {
+            console.error('Error al insertar usuario en la base de datos:', err);
+            res.status(500).send('Error interno del servidor');
+          } else {
+            console.log('Usuario registrado exitosamente');
+            res.redirect('/login'); // Redirigir a la página principal u otra página de confirmación
+          }
+        });
+      }
+    });
+  });
+
+  app.post('/login', (req, res) => {
+    // Verificar las credenciales del usuario (ejemplo simplificado)
+    const { usuario, contraseña } = req.body;
+    if (usuario === 'admin' && contraseña === 'admin') {
+      // Autenticación exitosa, establecer la sesión del usuario
+      req.session.isAuthenticated = true;
+      res.redirect('/');
+    } else {
+      res.status(401).send('Credenciales incorrectas');
+    }
+  });
+  
 
 // Servir imágenes convertidas
 app.use('/converted', express.static(path.join(__dirname, 'converted')));
