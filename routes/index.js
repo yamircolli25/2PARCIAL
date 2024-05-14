47ed218af4538cc87b8124be057507b22487df3c
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-05-13 23:09:00 -0600

    7.5

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 //Llama la vista del usuario
 router.get('/',(req, res) =>{
     res.render('index', {title: 'Convertidor de Imagenes'});
+    
 });
+
 
 module.exports = router;
